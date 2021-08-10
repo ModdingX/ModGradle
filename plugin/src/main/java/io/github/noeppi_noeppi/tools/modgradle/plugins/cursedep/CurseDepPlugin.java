6b6cff2485835483312cc66b86ed9bdd57acfbe8
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-08-10 20:16:42 +0200

    Deobfuscate curse dependencies

--- plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/plugins/cursedep/CurseDepPlugin.java
+++ plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/plugins/cursedep/CurseDepPlugin.java
@@ -1,5 +1,6 @@
 package io.github.noeppi_noeppi.tools.modgradle.plugins.cursedep;
 
+import net.minecraftforge.gradle.userdev.DependencyManagementExtension;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -14,6 +15,16 @@ public class CurseDepPlugin implements Plugin<Project> {
             r.setUrl("https://www.cursemaven.com/");
             r.content(c -> c.includeGroup("curse.maven"));
         });
-        project.getExtensions().create(CurseDependencyExtension.EXTENSION_NAME, CurseDependencyExtension.class, project);
+        DependencyManagementExtension ext = getDepExt(project);
+        if (ext == null) System.err.println("ModGradle cursedep can't find a DependencyManagementExtension. Curse dependencies can't be deobfuscated.");
+        project.getExtensions().create(CurseDependencyExtension.EXTENSION_NAME, CurseDependencyExtension.class, project, ext);
+    }
+    
+    private static DependencyManagementExtension getDepExt(Project project) {
+        try {
+            return project.getExtensions().getByType(DependencyManagementExtension.class);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
--- plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/plugins/cursedep/CurseDependencyExtension.java
+++ plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/plugins/cursedep/CurseDependencyExtension.java
@@ -1,23 +1,21 @@
 package io.github.noeppi_noeppi.tools.modgradle.plugins.cursedep;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.gson.JsonElement;
 import groovy.lang.GroovyObjectSupport;
 import io.github.noeppi_noeppi.tools.modgradle.ModGradle;
 import net.minecraftforge.gradle.common.util.MavenArtifactDownloader;
+import net.minecraftforge.gradle.userdev.DependencyManagementExtension;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
 
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.Callable;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -26,13 +24,17 @@ public class CurseDependencyExtension extends GroovyObjectSupport {
     public static final String EXTENSION_NAME = "curse";
 
     private final Project project;
+    
+    @Nullable
+    private final DependencyManagementExtension ext;
 
-    public CurseDependencyExtension(Project project) {
+    public CurseDependencyExtension(Project project, @Nullable DependencyManagementExtension ext) {
         this.project = project;
+        this.ext = ext;
     }
 
     public Dependency mod(int projectId, int fileId) {
-        return this.project.getDependencies().create(curseArtifact(getSlug(projectId), projectId, fileId));
+        return this.createDependency(curseArtifact(getSlug(projectId), projectId, fileId));
     }
 
     public Dependency pack(int projectId, int fileId) {
@@ -51,42 +53,44 @@ public class CurseDependencyExtension extends GroovyObjectSupport {
                 throw new IllegalStateException("Can't add curse ModPack dependency: Excludes must be int (project id) or String (slug)");
             }
         }
-        Callable<Set<File>> callable = () -> {
-            // Don't need a real slug here as it won't be visible anywhere
-            // Saves us one call to the API
-            File file = MavenArtifactDownloader.download(this.project, curseArtifact("cursepack", projectId, fileId), false);
-            if (file == null) {
-                throw new IllegalStateException("Can't resolve curse ModPack dependency: Failed to download manifest");
-            } else try {
-                ZipFile zipFile = new ZipFile(file);
-                ZipEntry entry = zipFile.getEntry("manifest.json");
-                if (entry == null) entry = zipFile.getEntry("/manifest.json");
-                if (entry == null)
-                    throw new IllegalStateException("Can't resolve curse ModPack dependency: Pack file contains no manifest.");
-                Reader reader = new InputStreamReader(zipFile.getInputStream(entry));
-                JsonElement json = ModGradle.GSON.fromJson(reader, JsonElement.class);
-                reader.close();
-                ImmutableSet.Builder<File> dependencies = ImmutableSet.builder();
-                for (JsonElement fileJson : json.getAsJsonObject().get("files").getAsJsonArray()) {
-                    int p = fileJson.getAsJsonObject().get("projectID").getAsInt();
-                    if (!idExcludes.contains(p)) {
-                        int f = fileJson.getAsJsonObject().get("fileID").getAsInt();
-                        String s = getSlug(p);
-                        if (!slugExcludes.contains(s)) {
-                            File curseFile = MavenArtifactDownloader.download(this.project, curseArtifact(s, p, f), false);
-                            if (curseFile == null) throw new IllegalStateException("Can't resolve curse ModPack dependency: Failed to download: " + curseArtifact(s, p, f));
-                            dependencies.add(curseFile);
-                        }
+        // Don't need a real slug here as it won't be visible anywhere
+        // Saves us one call to the API
+        File file = MavenArtifactDownloader.download(this.project, curseArtifact("cursepack", projectId, fileId), false);
+        if (file == null) {
+            throw new IllegalStateException("Can't create curse ModPack dependency: Failed to download manifest");
+        } else try {
+            ZipFile zipFile = new ZipFile(file);
+            ZipEntry entry = zipFile.getEntry("manifest.json");
+            if (entry == null) entry = zipFile.getEntry("/manifest.json");
+            if (entry == null) throw new IllegalStateException("Can't create curse ModPack dependency: Pack file contains no manifest.");
+            Reader reader = new InputStreamReader(zipFile.getInputStream(entry));
+            JsonElement json = ModGradle.GSON.fromJson(reader, JsonElement.class);
+            reader.close();
+            Configuration configuration = this.project.getConfigurations().create("cursepack_" + projectId + "_" + fileId + "_" + new Random().nextInt(1000));
+            for (JsonElement fileJson : json.getAsJsonObject().get("files").getAsJsonArray()) {
+                int p = fileJson.getAsJsonObject().get("projectID").getAsInt();
+                if (!idExcludes.contains(p)) {
+                    int f = fileJson.getAsJsonObject().get("fileID").getAsInt();
+                    String s = getSlug(p);
+                    if (!slugExcludes.contains(s)) {
+                        configuration.getDependencies().add(this.createDependency(curseArtifact(s, p, f)));
                     }
                 }
-                return dependencies.build();
-            } catch (IOException e) {
-                throw new IllegalStateException("Can't resolve curse ModPack dependency: " + e.getMessage(), e);
             }
-        };
-        return this.project.getDependencies().create(this.project.files(callable));
+            return this.project.getDependencies().create(configuration);
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't create curse ModPack dependency: " + e.getMessage(), e);
+        }
     }
 
+    private Dependency createDependency(Object obj) {
+        if (this.ext == null) {
+            return this.project.getDependencies().create(obj);
+        } else {
+            return this.ext.deobf(obj);
+        }
+    }
+    
     private static String curseArtifact(String slug, int projectId, int fileId) {
         return "curse.maven:" + slug + "-" + projectId + ":" + fileId;
     }

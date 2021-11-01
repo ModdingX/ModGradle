0b22e03b21ffa666a5c7aea994053c3180d98cf6
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-11-01 19:59:44 +0100

    Why windows why?

--- plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/util/ProcessUtil.java
+++ plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/util/ProcessUtil.java
@@ -4,16 +4,11 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Locale;
 
 public class ProcessUtil {
     
     public static void run(Path dir, String... command) throws IOException {
-        String[] quoted = new String[command.length];
-        for (int i = 0; i < command.length; i++) {
-            quoted[i] = quoteWin(command[i]);
-        }
-        ProcessBuilder pb = new ProcessBuilder(quoted);
+        ProcessBuilder pb = new ProcessBuilder(command);
         pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
         pb.redirectOutput(ProcessBuilder.Redirect.PIPE);
         pb.redirectError(ProcessBuilder.Redirect.INHERIT);
@@ -28,17 +23,6 @@ public class ProcessUtil {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new IOException("External process got interrupted.", e);
-        }
-    }
-
-    private static String quoteWin(String str) {
-        // Windows seems to not like special characters even if you use an argument
-        // array to start a process.
-        if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("win")) {
-            return '"' + str.replace("\"", "\\\"")
-                    .replace("\\", "\\\\") + '"';
-        } else {
-            return str;
         }
     }
 }

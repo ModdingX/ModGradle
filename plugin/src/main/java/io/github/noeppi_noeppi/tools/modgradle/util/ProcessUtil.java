afb527f06fdc01c1254f46e666e1bf241f9b9d7b
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-11-02 18:37:51 +0100

    Special case for the operating system that requires it. What os can this possibly be?

--- plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/util/ProcessUtil.java
+++ plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/util/ProcessUtil.java
@@ -4,11 +4,12 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Locale;
 
 public class ProcessUtil {
     
     public static void run(Path dir, String... command) throws IOException {
-        ProcessBuilder pb = new ProcessBuilder(command);
+        ProcessBuilder pb = new ProcessBuilder(buildCommand(command));
         pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
         pb.redirectOutput(ProcessBuilder.Redirect.PIPE);
         pb.redirectError(ProcessBuilder.Redirect.INHERIT);
@@ -23,6 +24,21 @@ public class ProcessUtil {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new IOException("External process got interrupted.", e);
+        }
+    }
+    
+    private static String[] buildCommand(String[] command) {
+        if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("win")) {
+            // Windows is a special case as always
+            // Give the command to cmd.exe seems to work, although it
+            // is really hacky. A better solution would be appreciated.
+            String[] newCommand = new String[command.length + 2];
+            newCommand[0] = "cmd";
+            newCommand[1] = "/c";
+            System.arraycopy(command, 0, newCommand, 2, command.length);
+            return newCommand;
+        } else {
+            return command;
         }
     }
 }

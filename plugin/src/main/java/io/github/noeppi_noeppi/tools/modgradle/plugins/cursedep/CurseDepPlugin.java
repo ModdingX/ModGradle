1c4f24a8d5b5f9e69aade0b3ca64bca84fe88426
MelanX <info@melanx.de>
2022-01-31 18:22:12 +0100

    re-add CurseDep plugin

---
+++ plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/plugins/cursedep/CurseDepPlugin.java
@@ -0,0 +1,44 @@
+package io.github.noeppi_noeppi.tools.modgradle.plugins.cursedep;
+
+import io.github.noeppi_noeppi.tools.cursewrapper.api.CurseWrapper;
+import net.minecraftforge.gradle.userdev.DependencyManagementExtension;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class CurseDepPlugin implements Plugin<Project> {
+
+    private static final CurseWrapper API = new CurseWrapper(URI.create("https://curse.melanx.de/"));
+
+    @Override
+    public void apply(Project project) {
+        project.getRepositories().maven(r -> {
+            r.setUrl("https://cfa2.cursemaven.com");
+            r.content(c -> c.includeGroup("curse.maven"));
+        });
+        DependencyManagementExtension ext = getDepExt(project);
+        project.getExtensions().create(CurseDependencyExtension.EXTENSION_NAME, CurseDependencyExtension.class, project, ext);
+    }
+
+    private static DependencyManagementExtension getDepExt(Project project) {
+        return project.getExtensions().getByType(DependencyManagementExtension.class);
+    }
+
+    public static String curseArtifact(int projectId, int fileId) {
+        return curseArtifact(getSlug(projectId), projectId, fileId);
+    }
+
+    public static String curseArtifact(String slug, int projectId, int fileId) {
+        return "curse.maven:" + slug + "-" + projectId + ":" + fileId;
+    }
+
+    public static String getSlug(int projectId) {
+        try {
+            return API.getSlug(projectId);
+        } catch (IOException e) {
+            return "unknown";
+        }
+    }
+}

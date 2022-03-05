663b948ec5fd13d93b52da11731726ebd47ac2b7
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-03-05 22:03:03 +0100

    Update librarian

--- plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/plugins/mapping/provider/SugarcaneProvider.java
+++ plugin/src/main/java/io/github/noeppi_noeppi/tools/modgradle/plugins/mapping/provider/SugarcaneProvider.java
@@ -1,16 +1,19 @@
 package io.github.noeppi_noeppi.tools.modgradle.plugins.mapping.provider;
 
 import net.minecraftforge.gradle.common.util.MavenArtifactDownloader;
+import net.minecraftforge.gradle.mcp.MCPRepo;
 import org.gradle.api.Project;
 import org.parchmentmc.feather.mapping.VersionedMappingDataContainer;
 import org.parchmentmc.librarian.forgegradle.ParchmentChannelProvider;
 import org.parchmentmc.librarian.forgegradle.ParchmentMappingVersion;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -28,7 +31,17 @@ public class SugarcaneProvider extends ParchmentChannelProvider {
     public Set<String> getChannels() {
         return Set.of("sugarcane");
     }
-    
+
+    @Nullable
+    @Override
+    public File getMappingsFile(MCPRepo mcpRepo, Project project, String channel, String mappingVersion) throws IOException {
+        ParchmentMappingVersion version = ParchmentMappingVersion.of(mappingVersion);
+        if (!Objects.equals(version.mcpVersion(), version.queryMcVersion())) {
+            System.err.println("Using SugarCane for " + version.queryMcVersion() + " on minecraft " + version.mcpVersion() + ". This is discouraged and will most likely result in compilation errors or subtle bugs.");
+        }
+        return super.getMappingsFile(mcpRepo, project, channel, mappingVersion);
+    }
+
     @Nonnull
     @Override
     protected Path getCacheBase(Project project) {
@@ -37,8 +50,8 @@ public class SugarcaneProvider extends ParchmentChannelProvider {
 
     @Nonnull
     @Override
-    protected File cacheParchment(Project project, String mcpVersion, String mappingsVersion, String ext) {
-        return MappingsProvider.getCacheFile(project, "sugarcane", mappingsVersion, mcpVersion, ext).toFile();
+    protected File cacheParchment(Project project, String queryMcVersion, String mcpVersion, String mappingsVersion, String ext) {
+        return MappingsProvider.getCacheFile(project, "sugarcane", (queryMcVersion.isEmpty() ? "null" : queryMcVersion) + "__" + mappingsVersion, mcpVersion, ext).toFile();
     }
 
     @Override

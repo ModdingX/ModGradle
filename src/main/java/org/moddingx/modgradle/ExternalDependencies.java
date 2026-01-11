ca4baf10bc18627a06c3dce72eb7480a09e6a4f8
noeppi_noeppi <noeppinoeppi@gmail.com>
2026-01-11 17:02:24 +0100

    update JavaDocletMeta version

--- src/main/java/org/moddingx/modgradle/ExternalDependencies.java
+++ src/main/java/org/moddingx/modgradle/ExternalDependencies.java
@@ -4,6 +4,6 @@ import org.moddingx.launcherlib.util.Artifact;
 
 public class ExternalDependencies {
     
-    public static final Artifact JAVA_DOCLET_META = Artifact.from("org.moddingx", "JavaDocletMeta", "2.0.0");
+    public static final Artifact JAVA_DOCLET_META = Artifact.from("org.moddingx", "JavaDocletMeta", "2.1.0");
     public static final String JAVA_DOCLET_META_DOCLET_CLASS = "org.moddingx.java_doclet_meta.Main";
 }
